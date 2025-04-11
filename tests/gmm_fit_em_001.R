46af7df0b7a91eccda1207d7eb445f5ebba48dac
Andrius Merkys <andrius.merkys@gmail.com>
2025-04-11 12:30:35 +0300

    Add a test for gmm_fit_em().

---
+++ tests/gmm_fit_em_001.R
@@ -0,0 +1,17 @@
+library( MixtureFitting )
+
+set.seed(42)
+p = c( 0.5, 0.5, 1.5, 10, 3, 1 )
+
+x = c( rnorm(2000 * p[1], p[3], p[5]), rnorm(2000 * p[2], p[4], p[6]) )
+init = gmm_init_vector( x, 2 )
+
+gf = gmm_fit_em( x, init, implementation = "C" )
+if( !all( abs( gf$p - c( 0.5, 0.5, 1.5, 10, 3, 1 ) ) < 0.1 ) ) {
+    stop( gf$p - p )
+}
+
+gf = gmm_fit_em( x, init, implementation = "R" )
+if( !all( abs( gf$p - c( 0.5, 0.5, 1.5, 10, 3, 1 ) ) < 0.1 ) ) {
+    stop( gf$p - p )
+}
