ce3496f459e55d4093701342c08d0b39a277db71
Andrius Merkys <andrius.merkys@gmail.com>
2026-05-12 12:01:10 +0300

    Add a test with faithful data and results taken from Lin et al. (2007), Statistica Sinica 17, 909-92, Table 3.

---
+++ tests/snmm_fit_em_002.R
@@ -0,0 +1,15 @@
+library( datasets )
+library( MixtureFitting )
+
+x = faithful[,1]
+# Resulting values taken from Lin et al. (2007), Statistica Sinica 17, 909-92, Table 3
+p = c( 0.3487, 1 - 0.3487, 1.7267, 4.8026, 0.3801, 0.6857, 5.8026, -3.4951 )
+
+sf = snmm_init_vector_kmeans( x, 2 )
+for (i in 1:5000) {
+    sf = snmm_fit_em(x, sf)
+}
+
+if( !all( abs( sf - p ) < 0.1 ) ) {
+    stop( sf )
+}
